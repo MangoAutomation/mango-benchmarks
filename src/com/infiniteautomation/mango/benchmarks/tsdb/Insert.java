c45492e6a209380e02bd9146d965db1adea30d21
Jared Wiltshire <jazdw@users.noreply.github.com>
2021-10-22 10:05:48 -0600

    Insert in batches

--- src/com/infiniteautomation/mango/benchmarks/tsdb/Insert.java
+++ src/com/infiniteautomation/mango/benchmarks/tsdb/Insert.java
@@ -9,6 +9,8 @@ import java.util.List;
 import java.util.Random;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
+import java.util.function.Supplier;
+import java.util.stream.Stream;
 
 import org.junit.Test;
 import org.openjdk.jmh.annotations.Benchmark;
@@ -17,16 +19,18 @@ import org.openjdk.jmh.annotations.Fork;
 import org.openjdk.jmh.annotations.Measurement;
 import org.openjdk.jmh.annotations.Mode;
 import org.openjdk.jmh.annotations.OutputTimeUnit;
+import org.openjdk.jmh.annotations.Param;
 import org.openjdk.jmh.annotations.Scope;
 import org.openjdk.jmh.annotations.Setup;
 import org.openjdk.jmh.annotations.State;
 import org.openjdk.jmh.annotations.Warmup;
-import org.openjdk.jmh.infra.Blackhole;
 import org.openjdk.jmh.runner.RunnerException;
 import org.openjdk.jmh.runner.options.CommandLineOptionException;
 import org.openjdk.jmh.runner.options.CommandLineOptions;
 import org.openjdk.jmh.runner.options.OptionsBuilder;
 
+import com.serotonin.m2m2.db.dao.BatchPointValue;
+import com.serotonin.m2m2.db.dao.BatchPointValueImpl;
 import com.serotonin.m2m2.rt.dataImage.PointValueTime;
 import com.serotonin.m2m2.vo.DataPointVO;
 
@@ -48,11 +52,15 @@ public class Insert extends TsdbBenchmark {
     }
 
     @State(Scope.Thread)
-    public static class InsertState {
+    public static class InsertParams {
+
+        @Param({"1", "100", "1000"})
+        int batchSize;
 
         final Random random = new Random();
         List<DataPointVO> points;
-        int index = 0;
+        long index = 0;
+        final long timeIncrement = 5000L;
 
         @Setup
         public void setup(TsdbMockMango mango) throws ExecutionException, InterruptedException {
@@ -62,29 +70,39 @@ public class Insert extends TsdbBenchmark {
         public PointValueTime newValue(long timestamp) {
             return new PointValueTime(random.nextDouble(), timestamp);
         }
+
+        public Stream<BatchPointValue> generateData(DataPointVO point, long startTime) {
+            return Stream.generate(new Supplier<BatchPointValue>() {
+                int count;
+
+                @Override
+                public BatchPointValue get() {
+                    return new BatchPointValueImpl(point, newValue(startTime + count++ * timeIncrement));
+                }
+            }).limit(batchSize);
+        }
     }
 
     @Benchmark
-    public void insert(TsdbMockMango mango, InsertState insertState, Blackhole blackhole) {
-        long timestamp = insertState.index * 5000L;
-        for (DataPointVO point : insertState.points) {
-            PointValueTime v = mango.pvDao.savePointValueSync(point, insertState.newValue(timestamp), null);
-            blackhole.consume(v);
+    public void insert(TsdbMockMango mango, InsertParams insertParams) {
+        long startTime = insertParams.index * insertParams.timeIncrement * insertParams.batchSize;
+        for (DataPointVO point : insertParams.points) {
+            mango.pvDao.savePointValues(insertParams.generateData(point, startTime));
         }
-        insertState.index++;
+        insertParams.index++;
     }
 
     @Benchmark
-    public void backdates(TsdbMockMango mango, InsertState insertState, Blackhole blackhole) {
-        long timestamp = insertState.index * 5000L;
-        // every 1000 iterations insert a backdated value
-        if (insertState.index % 1000 == 0) {
-            timestamp -= 1000 * 5000L;
+    public void withBackdates(TsdbMockMango mango, InsertParams insertParams) {
+        long index = insertParams.index;
+        // every 100 iterations insert some backdated values, this leaves holes and back-fills them without overwriting
+        if (index % 100 == 0) {
+            index -= 100;
         }
-        for (DataPointVO point : insertState.points) {
-            PointValueTime v = mango.pvDao.savePointValueSync(point, insertState.newValue(timestamp), null);
-            blackhole.consume(v);
+        long startTime = index * insertParams.timeIncrement * insertParams.batchSize;
+        for (DataPointVO point : insertParams.points) {
+            mango.pvDao.savePointValues(insertParams.generateData(point, startTime));
         }
-        insertState.index++;
+        insertParams.index++;
     }
 }
